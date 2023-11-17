cc006732a389c7332edc4793728429b969c9471b
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-11-16 19:59:48 -0500

    stash

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -12,7 +12,8 @@ function App() {
     "date": "July 4, 2023",
     "body": "I am the president yo",
     "likes":"50",
-    "reposts":"60"
+    "reposts":"60",
+    "tags":["President", "America", "USA","USA","USA","USA","USA"]
   }
 
   const temp_info2 = {
@@ -21,7 +22,8 @@ function App() {
     "date": "July 4, 2023",
     "body": "Hey, have you played metal gear? Do you know what metal gear is? Let me know if you played it ok? Yes? metal gear.",
     "likes":"50",
-    "reposts":"60"
+    "reposts":"60",
+    "tags":["President", "America", "USA"]
   }
 
   const temp_info3 = {
@@ -30,7 +32,8 @@ function App() {
     "date": "July 4, 2023",
     "body": "Elaphant",
     "likes":"50",
-    "reposts":"60"
+    "reposts":"60",
+    "tags":["President", "America", "USA"]
   }
   return (
     <>
@@ -44,10 +47,10 @@ function App() {
           <h4 className="filter-option">Anon.</h4>
           <h4 className="filter-option">Following.</h4>
         </div>
-        <ViewCard pfp={temp_info1["pfp"]} author={temp_info1["author"]} body={temp_info1["body"]} color={"#fa000055"} likes={temp_info1["likes"]}/>
-        <ViewCard pfp={temp_info2["pfp"]} author={temp_info2["author"]} body={temp_info2["body"]} color={"#0000fa55"} likes={temp_info2["likes"]}/>
-        <ViewCard pfp={temp_info3["pfp"]} author={temp_info3["author"]} body={temp_info3["body"]} color={"#00ff005b"} likes={temp_info3["likes"]}/>
-        <ViewCard pfp={temp_info3["pfp"]} author={temp_info3["author"]} body={temp_info3["body"]} color={"#ffff0055"} likes={temp_info3["likes"]}/>
+        <ViewCard pfp={temp_info1["pfp"]} author={temp_info1["author"]} body={temp_info1["body"]} color={"#fa000055"} likes={temp_info1["likes"]} tags={temp_info1["tags"]}/>
+        <ViewCard pfp={temp_info2["pfp"]} author={temp_info2["author"]} body={temp_info2["body"]} color={"#0000fa55"} likes={temp_info2["likes"]} tags={temp_info1["tags"]}/>
+        <ViewCard pfp={temp_info3["pfp"]} author={temp_info3["author"]} body={temp_info3["body"]} color={"#00ff005b"} likes={temp_info3["likes"]} tags={temp_info1["tags"]}/>
+        <ViewCard pfp={temp_info3["pfp"]} author={temp_info3["author"]} body={temp_info3["body"]} color={"#ffff0055"} likes={temp_info3["likes"]} tags={temp_info1["tags"]}/>
       </div>
       
     </div>
--- front_end/src/components/view-post-card.js
+++ front_end/src/components/view-post-card.js
@@ -1,7 +1,30 @@
 import '../static/css/post-card.css';
 import heart from '../static/images/heart.png';
+import { useState } from "react";
+
+const Comments = (comment)=>{
+    if(comment){
+        return(
+            <div className='comment-button'>
+                <div className='user-comment-container'>
+                    <input className='user-comment'type='text' placeholder="Add Comment"></input>
+                </div>
+                <div className='comments'>
+                    <img className='card-pfp-img comment-pfp-img'></img>
+                    <h5 className='comment-body'></h5>
+                </div>
+            </div>
+        )
+    }
+    return(
+        <>
+            FALSE
+        </>
+    )
+}
+
 const ViewCard = (props)=> {
-    
+    let [commentClick, setCommentClick] = useState(true);
   return (
     <>
         <div className='card'>
@@ -11,8 +34,14 @@ const ViewCard = (props)=> {
                     <h4 className='card-author'>{props.author}</h4>
                 </div>
                 <div className="tags">
-
+                    {props.tags.map((tag) =>(
+                        <div className="tag">
+                            {tag}
+                        </div>
+                    ))}
                 </div>
+                <img className="card-pfp-img likes" src={heart}/>
+                <p className="like-count">{props.likes}</p>
                 <img className="card-pfp-img likes" src={heart}/>
                 <p className="like-count">{props.likes}</p>
                 
@@ -21,6 +50,7 @@ const ViewCard = (props)=> {
                 <h3>{props.body}</h3>
             </div>
         </div>
+        
     </>
   );
 }
