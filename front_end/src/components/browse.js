0c92a574f7c1802304e5278bdc1cd50fd0e67dee
MartinLahoumh <martinlahoumh103102@gmail.com>
2023-11-29 12:12:08 -0500

    changed design of browse tab

--- front_end/src/components/browse.js
+++ front_end/src/components/browse.js
@@ -9,22 +9,23 @@ const Browse = (props)=> {
         console.log(e.target.id);
         if(open == false){
             setOpen(true);
-            document.getElementById('browse-wrapper').style = "margin-left:0px; transition: 0.3s;";
+            document.getElementById('browse-wrapper').style = "margin-left:4%; transition: 0.3s;";
+            
         }
         else if(open == true){
             setOpen(false);
-            document.getElementById('browse-wrapper').style = "margin-left:70%; transition: 0.3s;";
+            document.getElementById('browse-wrapper').style = "margin-left:71%; transition: 0.3s;";
         }
     }
     //Recieve the info from props CONTINUE HERE!!!
   return (
     <div className='browse-wrapper' id='browse-wrapper'>  
-        <button onClick={browseOpen} className='browse-button'>
+        <button onClick={browseOpen} className='browse-button' id='browse-button'>
                 
         </button>
-        <div style={{backgroundColor:'red'}}className='browse-container-comp'>
+        <div className='browse-container-comp'>
                 <h1>Browse</h1>
-                <div className='sub-container'>
+                <div className='sub-container suggested'>
                     <h3>Suggested</h3>
                     <div className='comment-button-open suggested-profile-container'>
                         {/*Insert profile cards here */}
@@ -34,7 +35,7 @@ const Browse = (props)=> {
                         <ProfileCard  />
                     </div>
                 </div>
-            <div className='sub-container'>
+            <div className='sub-container trending'>
                 <h3>Trending Users</h3>
                 <div className='comment-button-open suggested-profile-container'>
                     {/*Insert profile cards here */}
@@ -44,7 +45,7 @@ const Browse = (props)=> {
                     <ProfileCard  />
                 </div>
             </div>
-            <div className='sub-container'>
+            <div className='sub-container jobs'>
                 <h3>Job Listings</h3>
                 {/*Insert Job cards here */}
                 <ProfileCard  />
