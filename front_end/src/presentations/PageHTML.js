86e8187eb0fb21ce009e5ba652f37d7b4fd1c988
Sorbies <ericlo_kidz@hotmail.com>
2023-12-04 14:02:53 -0500

    organized page content

--- front_end/src/presentations/PageHTML.js
+++ front_end/src/presentations/PageHTML.js
@@ -1,45 +1,19 @@
-//assets
-import biden_pfp from '../static/images/biden-pfp.jpg';
-import kojima_pfp from '../static/images/kojima-pfp.jpg';
-import mario_pfp from '../static/images/mario-pfp.jpg';
-
 //components
-import CreatePostCtrl from "../components/CreatePostCtrl";
-import ViewCard from "../components/view-post-card";
-import JobCard from "../components/job-card";
+import JobsCtrl from '../components/JobsCtrl';
+import ForYouCtrl from "../components/ForYouCtrl";
 
 function PageHTML(props) {
-    let pageContent = <></>;
-
     //display logic
-    // const showPostCard = cookies.loggedIn == true ? postCard : <></>;
 
     const suggested_users = [] //Get from the backend and fill it here
     const trending_users = [] //Get from the backend and fill it here
 
-    const tempRequirments = ['Good work ethic', 'team player'];
-
+    let pageContent = <></>;
     if (props.page == 'ForYou') {
-        const colors = ["#fa000055", "#0000fa55", "#00ff005b", "#ffff0055"]
-        pageContent = (
-            <>
-                <CreatePostCtrl info={props.info} />
-                {props.posts.map((post) => (
-                    <ViewCard pfp={post["pfp"]} author={post["author"]} body={post["body"]} color={colors[Math.floor(Math.random() * (4))]} likes={post["likes"]} tags={post["tags"]} />
-                ))}
-            </>
-        )
+        pageContent = <ForYouCtrl info={props.info} whichCookies={props.whichCookies}/>
     }
     else if (props.page == 'Jobs') {
-        pageContent = (
-            <div className="job-posts-container">
-                {/*Like with users, once you get the jobs, map it. I just made it temp values for demonstration purposes */}
-                <JobCard jobIcon={biden_pfp} position="President" requirements={tempRequirments} />
-                <JobCard jobIcon={biden_pfp} position="President" requirements={tempRequirments} />
-                <JobCard jobIcon={biden_pfp} position="President" requirements={tempRequirments} />
-                <JobCard jobIcon={biden_pfp} position="President" requirements={tempRequirments} />
-            </div>
-        )
+        pageContent = <JobsCtrl />
     }
 
     return (
@@ -50,7 +24,6 @@ function PageHTML(props) {
                     <h4 id="Following" className="filter-option" onClick={props.changePage}>Following.</h4>
                     <h4 id="Jobs" className="filter-option" onClick={props.changePage}>Jobs.</h4>
                 </div>
-                Bingus {props.test}
                 {pageContent}
             </div>
         </>
