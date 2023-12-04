428bb1fd0744df010ad5217dd879ed13d7c59144
Sorbies <ericlo_kidz@hotmail.com>
2023-12-04 14:04:33 -0500

    bulk of the post loading logic now implemented

---
+++ front_end/src/presentations/ForYouHTML.js
@@ -0,0 +1,39 @@
+//components
+import CreatePostCtrl from "../components/CreatePostCtrl";
+import ViewCard from "../components/view-post-card";
+
+function ForYouHTML(props) {
+    const colors = ["#fa000055", "#0000fa55", "#00ff005b", "#ffff0055"]
+
+    return (
+        <>
+            {/* component that allows you to make a new post */}
+            <CreatePostCtrl info={props.info} triggerGetInitialPosts={props.triggerGetInitialPosts} />
+
+            {/* turn each post info into a post card component */}
+            {props.postsInfo.map((post, index) => {
+                const postContent = post[0];
+                const userContent = props.usersInfo[index][0];
+                console.log(postContent, userContent);
+
+                return (
+                    <>
+                        <ViewCard pfp={userContent["avatar"]}
+                            author={userContent["username"]}
+                            body={postContent["text"]}
+                            color={colors[Math.floor(Math.random() * (4))]}
+                            likes={postContent["likes"]}
+                            dislikes={postContent["dislikes"]}
+                            tags={postContent["keywords"]} 
+                            views={postContent["views"]}
+                            date={postContent["time_posted"]}/>
+
+                    </>
+                );
+            }
+            )}
+        </>
+    );
+}
+
+export default ForYouHTML;
