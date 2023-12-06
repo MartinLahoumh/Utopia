8119eee491a57c61d4ea107f4303e7fc849384e2
Sorbies <ericlo_kidz@hotmail.com>
2023-12-06 16:46:00 -0500

    commenting code

--- front_end/src/db methods/dbCheckDislike.js
+++ front_end/src/db methods/dbCheckDislike.js
@@ -1,5 +1,5 @@
 function dbCheckDislike(info, targetid) {
-  console.log(info, targetid);
+  //console.log(info, targetid);
   return info.includes(targetid.toString());
 }
 
--- front_end/src/db methods/dbCheckLike.js
+++ front_end/src/db methods/dbCheckLike.js
@@ -1,5 +1,5 @@
 function dbCheckLike(info, targetid) {
-  console.log(info, targetid);
+  //console.log(info, targetid);
   return info.includes(targetid.toString());
 }
 
