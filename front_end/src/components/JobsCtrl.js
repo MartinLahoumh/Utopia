a96616d6175fda5edfd71269e4618d445eb160e0
joewadk <jkabir000@citymail.cuny.edu>
2023-12-06 22:20:31 -0500

    display jobs when valid data is added

--- front_end/src/components/JobsCtrl.js
+++ front_end/src/components/JobsCtrl.js
@@ -1,66 +1,88 @@
-//components
-import JobsHTML from "../presentations/JobsHTML";
-
 import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 
-import {   dbGetUserInfo } from "../db methods/dbGetUserInfo";
+//methods
 import { dbGetPosts } from "../db methods/dbGetPosts";
 import { dbGetPostInfo } from "../db methods/dbGetPostInfo";
+import { dbGetUserInfo } from "../db methods/dbGetUserInfo";
 
-function JobsCtrl(props) {
- 
+//components
+import JobsHTML from "../presentations/JobsHTML";
+
+// Mock job listings
+const mockJobListings = [
+    {
+        id: 1,
+        title: "Software Engineer",
+        company: "Tech Corp",
+        location: "San Francisco, CA",
+        description: "Seeking a skilled software engineer with 5 years of experience in web development."
+    },
+    {
+        id: 2,
+        title: "Graphic Designer",
+        company: "Creative Studio",
+        location: "New York, NY",
+        description: "Looking for a creative graphic designer proficient in Adobe Suite and with a great portfolio."
+    },
+    {
+        id: 3,
+        title: "Data Analyst",
+        company: "Data Inc.",
+        location: "Austin, TX",
+        description: "Data analyst needed to analyze large datasets and provide insights. SQL and Python skills required."
+    },
+    // ...add more listings as needed
+];
+
+function JobsCtrl({ info, triggerGetUserInfo, whichCookies }) {
     const [cookies] = useCookies();
 
-    // State for posts pagination
-    const [before, setBefore] = useState(null);
+    // State for controlling data fetching
+    const [requestGetInitialJobs, setRequestGetInitialJobs] = useState(false);
+    const [requestGetJobInfo, setRequestGetJobInfo] = useState(false);
 
     // States for post data
     const [posts, setPosts] = useState([]);
     const [postsInfo, setPostsInfo] = useState([]);
     const [usersInfo, setUsersInfo] = useState([]);
 
-    // States for controlling data fetching
-    const [requestGetInitialJobs, setRequestGetInitialJobs] = useState(false);
-    const [requestGetJobInfo, setRequestGetJobInfo] = useState(false);
+    // State to store job listings
+    const [jobListings, setJobListings] = useState([]);
+
+    // Function to trigger getting initial jobs
+    function triggerGetInitialJobs() {
+        setRequestGetInitialJobs(true);
+    }
 
     // Effect for retrieving initial job posts
     useEffect(() => {
         async function handleGetInitialJobs() {
             if (requestGetInitialJobs) {
-                const [uid, key] = props.whichCookies();
+                const uid = cookies.uid;
+                const key = cookies.key;
 
                 try {
-                    const [jobPosts, beforeResult, error] = await dbGetPosts(uid, key, 10, null, ["JOB"]);
+                    const [jobPosts, beforeResult] = await dbGetPosts(uid, key, 10, null, ["JOB"]);
                     
-                    setBefore(beforeResult);
                     setPosts(jobPosts);
                     setRequestGetJobInfo(true);
                 } catch (error) {
-                    console.log(error);
+                    console.error(error); 
                 } finally {
                     setRequestGetInitialJobs(false);
                 }
             }
         }
         handleGetInitialJobs();
-    }, [requestGetInitialJobs, props.whichCookies]);
-
-    // Triggering the initial jobs fetching
-    useEffect(() => {
-        setRequestGetInitialJobs(true);
-    }, []);
-
-    // Function to trigger getting jobs manually
-    function triggerGetInitialJobs() {
-        setRequestGetInitialJobs(true);
-    }
+    }, [requestGetInitialJobs, cookies, whichCookies]);
 
     // Effect for getting job posts information
     useEffect(() => {
         async function handleGetJobsInfo() {
             if (requestGetJobInfo) {
-                const [uid, key] = props.whichCookies();
+                const uid = cookies.uid;
+                const key = cookies.key;
 
                 try {
                     let newPostsInfo = [];
@@ -77,23 +99,29 @@ function JobsCtrl(props) {
                     setPostsInfo(newPostsInfo);
                     setUsersInfo(newUsersInfo);
                 } catch (error) {
-                    console.log(error);
-                } finally { 
+                    console.error(error);
+                } finally {
                     setRequestGetJobInfo(false);
                 }
             }
         }
         handleGetJobsInfo();
-    }, [requestGetJobInfo, posts, props.whichCookies]);
+    }, [requestGetJobInfo, posts, cookies, whichCookies]);
+
+    // Simulate fetching job listings
+    useEffect(() => {
+        setJobListings(mockJobListings);
+    }, []);
 
     return (
         <JobsHTML
-            info={props.info}
+            info={info}
+            jobListings={jobListings} // Pass job listings to JobsHTML
             triggerGetInitialJobs={triggerGetInitialJobs}
             postsInfo={postsInfo}
             usersInfo={usersInfo}
-            whichCookies={props.whichCookies}
-            triggerGetUserInfo={props.triggerGetUserInfo}
+            whichCookies={whichCookies}
+            triggerGetUserInfo={triggerGetUserInfo}
         />
     );
 }
